80388879e38bb42838971d0131193beedf51f9c9
Reda <mahmoudreda457@gmail.com>
2026-05-22 10:02:24 +0300

    Calculating investment

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.modal';
 
 @Component({
   selector: 'app-user-input',
@@ -9,18 +10,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit () {
-    // console.log(event.target.value);
     console.log('Submitted!');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
 
+    // console.log(event.target.value);
+    // console.log(this.enteredInitialInvestment);
+    // console.log(this.enteredAnnualInvestment);
+    // console.log(this.enteredExpectedReturn);
+    // console.log(this.enteredDuration);
   }
 }
